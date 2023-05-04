fce3b6393b80ec25fb5e4e51dc96dbeabb4d5ccd
martalexaa <63308375+martalexaa@users.noreply.github.com>
2023-05-04 10:25:12 +0200

    Clean up

--- src/components/profile-view/delete-user.jsx
+++ src/components/profile-view/delete-user.jsx
@@ -5,7 +5,7 @@ export const DeleteUser = () => {
 
   const user = useSelector((state) => state.user.user);
   const token = useSelector((state) => state.token.token);
-  
+
   const handleDeregister = () => {
     const userWarning = confirm(
       `You are going to delete your account. All information will be lost and cannot be recovered. Are you sure?`
@@ -14,41 +14,41 @@ export const DeleteUser = () => {
     userWarning === false
       ? alert('Great decision. Keep choosing your favorite movies')
       : fetch(
-          `https://martalexa-myflix.onrender.com/users/${user.Username}`,
-          {
-            method: 'DELETE',
-            headers: {
-              Authorization: `Bearer ${token}`,
-              'Content-Type': 'application/json',
-            },
+        `https://martalexa-myflix.onrender.com/users/${user.Username}`,
+        {
+          method: 'DELETE',
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+        .then((response) => {
+          if (response.ok) {
+            alert('Account successfully deleted');
+            localStorage.clear();
+            window.location.reload();
+          } else {
+            alert('Something went wrong');
           }
-        )
-          .then((response) => {
-            if (response.ok) {
-              alert('Account successfully deleted');
-              localStorage.clear();
-              window.location.reload();
-            } else {
-              alert('Something went wrong');
-            }
-          })
-          .catch((e) => console.log(e));
+        })
+        .catch((e) => console.log(e));
   };
 
   return (
-      <>
-        <Col>
+    <>
+      <Col>
         <Row>
           <h6 className='mt-5 mb-2'>You no longer want to be registered for the Top Rated Movies?</h6>
-        <Button
-          onClick={() => handleDeregister(user.Username)}
-          className='delete-button mb-5'
-          variant='danger'
-        >
-          Delete Account
-        </Button>
+          <Button
+            onClick={() => handleDeregister(user.Username)}
+            className='delete-button mb-5'
+            variant='danger'
+          >
+            Delete Account
+          </Button>
         </Row>
-        </Col>
-      </>
+      </Col>
+    </>
   );
 };
